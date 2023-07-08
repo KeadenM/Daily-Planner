0683b9398f57ee27d88061d97f5d61b10d169ddb
KeadenM <keadenmeacham@gmail.com>
2023-07-08 16:38:50 -0600

    started variables for time keeping and scheudling

--- script.js
+++ script.js
@@ -14,5 +14,22 @@ var parentDivID = parentDiv.attr('id');
 localStorage.setItem(parentDivID, setSchedule);
 });
 
+var time =parseInt(dayjs().format('H'));
+$('.time-block').each(function() {
+
+var scheduleID = $(this).attr('id').split('-');
+
+var scheduleHour = parseInt(scheduleID[1]);
+
+var scheduleAmPM = scheduleID[2];
+
+var parentDiv = $(this);
+
+var textarea = parentDiv.find('textarea');
+
+var parentDivID = parentDiv.attr('id');
+
+
+})
 
 });
