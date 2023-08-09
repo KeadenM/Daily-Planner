9d18cfed23e6b314b133ee3c1e26c0d5491d190d
KeadenM <keadenmeacham@gmail.com>
2023-08-08 21:36:54 -0600

    made code easier to read

--- script.js
+++ script.js
@@ -1,31 +1,57 @@
 $(function () {
   
   var hr9El = document.getElementById("hour-9");
+
   var hr9Inpt = document.getElementById("hour-9").childNodes[3];
+
   var hr9Bttn = document.getElementById("hour-9").childNodes[5];
+
   var hr10El = document.getElementById("hour-10");
+
   var hr10Inpt = document.getElementById("hour-10").childNodes[3];
+
   var hr10Bttn = document.getElementById("hour-10").childNodes[5];
+
   var hr11El = document.getElementById("hour-11");
+
   var hr11Inpt = document.getElementById("hour-11").childNodes[3];
+
   var hr11Bttn = document.getElementById("hour-11").childNodes[5];
+
   var hr12El = document.getElementById("hour-12");
+
   var hr12Inpt = document.getElementById("hour-12").childNodes[3];
+
   var hr12Bttn = document.getElementById("hour-12").childNodes[5];
+
   var hr1El = document.getElementById("hour-1");
+
   var hr1Inpt = document.getElementById("hour-1").childNodes[3];
+
   var hr1Bttn = document.getElementById("hour-1").childNodes[5];
+
   var hr2El = document.getElementById("hour-2");
+
   var hr2Inpt = document.getElementById("hour-2").childNodes[3];
+
   var hr2Bttn = document.getElementById("hour-2").childNodes[5];
+
   var hr3El = document.getElementById("hour-3");
+
   var hr3Inpt = document.getElementById("hour-3").childNodes[3];
+
   var hr3Bttn = document.getElementById("hour-3").childNodes[5];
+
   var hr4El = document.getElementById("hour-4");
+
   var hr4Inpt = document.getElementById("hour-4").childNodes[3];
+
   var hr4Bttn = document.getElementById("hour-4").childNodes[5];
+
   var hr5El = document.getElementById("hour-5");
+
   var hr5Inpt = document.getElementById("hour-5").childNodes[3];
+  
   var hr5Bttn = document.getElementById("hour-5").childNodes[5];
 .
   var hrArray = [hr9El, hr10El, hr11El, hr12El, hr1El, hr2El, hr3El, hr4El, hr5El];
